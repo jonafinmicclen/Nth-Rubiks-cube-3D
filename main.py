446de7e0d80144c069f81b53e9bbd50d868089f0
Jonathan McClen <jonathangmcclen@gmail.com>
2023-12-30 02:37:13 +0000

    colors are now correct

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import random
+import numpy as np
+
+#define screen dimensions
+WIDTH = 1920
+HEIGHT = 1080
+
+#define colors
+WHITE = (255,255,255)
+RED = (137,18,20)
+BLUE = (13,72,172)
+ORANGE = (255,85,37)
+GREEN = (25,155,76)
+YELLOW = (254,213,47)
+
+all_colors = np.array([WHITE, GREEN, YELLOW, BLUE, RED, ORANGE])/255
 
 class Cube:
-    def __init__(self, position):
+    def __init__(self, position, colors):
         self.vertices = (
             (1, -1, -1),
             (1, 1, -1),
@@ -26,23 +40,14 @@ class Cube:
             (4, 0, 3, 6)
         )
 
-        # Each face has 9 distinct colors
-        self.colors = [
-            (random.random(), random.random(), random.random()),  # Color for the first face
-            (random.random(), random.random(), random.random()),  # Color for the second face
-            (random.random(), random.random(), random.random()),  # Color for the third face
-            (random.random(), random.random(), random.random()),  # Color for the fourth face
-            (random.random(), random.random(), random.random()),  # Color for the fifth face
-            (random.random(), random.random(), random.random())   # Color for the sixth face
-        ]
-
+        self.colors = colors
         self.position = position
 
     def draw(self):
         glBegin(GL_QUADS)
 
         for i, surface in enumerate(self.surfaces):
-            for j, vertex in enumerate(surface):
+            for vertex in surface:
                 glColor3fv(self.colors[i])  # Set the unique color for each face
                 glVertex3fv((self.vertices[vertex][0] + self.position[0],
                              self.vertices[vertex][1] + self.position[1],
@@ -50,28 +55,39 @@ class Cube:
 
         glEnd()
 
+class CubeSet:
+    def __init__(self, size, spacing):
+        self.cubes = []
+
+        for x in range(0, size):
+            for y in range(0, size):
+                for z in range(0, size):
+                    colors = all_colors
+                    cube = Cube(position=(x * spacing, y * spacing, z * spacing), colors=colors)
+                    self.cubes.append(cube)
+
+    def draw(self):
+        for cube in self.cubes:
+            cube.draw()
+
+#Initialise pygame window with opengl
 pygame.init()
-display = (800, 600)
+display = (WIDTH, HEIGHT)
 pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-pygame.display.set_caption('Colored Cubes')
+pygame.display.set_caption('Rubiks')
 
+#Position perspective
 gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-glTranslatef(0.0, 0.0, -15)
+glTranslatef(0.0, 0.0, -20)
 
-# Enable depth testing
+# Enable depth testing so only correct faces are drawn
 glEnable(GL_DEPTH_TEST)
 
-# Create a 3x3x3 grid of cubes with the original spacing
-cubes = []
-spacing = 2
-for x in range(-1, 2):
-    for y in range(-1, 2):
-        for z in range(-1, 2):
-            cube = Cube(position=(x * spacing, y * spacing, z * spacing))
-            cubes.append(cube)
+# Create a set of cubes with the original spacing
+cube_set = CubeSet(size=3, spacing=2)
 
 clock = pygame.time.Clock()
-fps = 60
+fps = 30
 
 while True:
     for event in pygame.event.get():
@@ -82,8 +98,6 @@ while True:
     glRotatef(1, 3, 1, 1)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    for cube in cubes:
-        cube.draw()
-
+    cube_set.draw()
     pygame.display.flip()
     clock.tick(fps)
